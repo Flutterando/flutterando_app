04d61210bd2f4900f94d5709a24c0e55df68e15e
Wellington Santos <wellington.santos@genial.com.vc>
2025-04-12 10:04:22 -0300

    feature: implement splash screen

--- app/lib/ui/splash/splash_page.dart
+++ app/lib/ui/splash/splash_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:routefly/routefly.dart';
 
 import '../../app_widget.dart';
+import '../design_system/theme/theme.dart';
+import '../design_system/widgets/svg_image_widget.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -10,18 +12,54 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController animeCtrl;
+  Animation<Color?>? colorAnimation;
+
   @override
   void initState() {
     super.initState();
 
-    Future.delayed(
-      const Duration(seconds: 1),
-    ).then((_) => Routefly.navigate(routePaths.auth.login));
+    animeCtrl = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      colorAnimation = ColorTween(
+        begin: context.colors.primaryColor,
+        end: context.colors.backgroundColor,
+      ).animate(
+        CurvedAnimation(
+          parent: animeCtrl,
+          curve: const Interval(0.5, 1.0, curve: Curves.easeInOut),
+        ),
+      );
+
+      animeCtrl.forward();
+      Future.delayed(
+        const Duration(seconds: 2),
+      ).then((_) => Routefly.navigate(routePaths.auth.login));
+    });
+  }
+
+  @override
+  void dispose() {
+    animeCtrl.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AnimatedBuilder(
+      animation: animeCtrl,
+      builder: (context, _) {
+        return Scaffold(
+          backgroundColor: colorAnimation?.value ?? Colors.black,
+          body: Center(child: SvgImage.logoWhite.image()),
+        );
+      },
+    );
   }
 }
