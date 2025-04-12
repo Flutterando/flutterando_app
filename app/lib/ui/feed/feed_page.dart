7bf5f651ceef894920ba8790d68c3761a324bd00
edilson.tavares <edytavares66@gmail.com>
2025-04-12 12:36:02 -0100

    feature: adicionar widget LinkPost e dependência de imagem em cache

--- app/lib/ui/feed/feed_page.dart
+++ app/lib/ui/feed/feed_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../design_system/constants/spaces.dart';
+import '../design_system/widgets/link_post_widget.dart';
+
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
 
@@ -13,9 +16,15 @@ class _FeedPageState extends State<FeedPage> {
     return const Scaffold(
       body: SafeArea(
         child: Column(
-          spacing: 12.0,
+          spacing: Spaces.m,
           children: [
             Text('Tela do feed'),
+            LinkPostWidget(
+              postDescription:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nisl nunc egestas nunc, eget lacinia nunc nisl eget nunc.',
+              imageUrl:
+                  'https://media.licdn.com/dms/image/v2/D4E22AQG23nwGm5GN9A/feedshare-shrink_2048_1536/B4EZWzWwX4HgAs-/0/1742470843949?e=1747267200&v=beta&t=FnsBSTJQ6zTZn9aSksEC8zFBpH-GBO8yV_NjI0LXhe8',
+            ),
           ],
         ),
       ),
