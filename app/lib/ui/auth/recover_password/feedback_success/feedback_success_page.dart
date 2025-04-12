5f41a5b4c7c52bfef8a7af2cbfa43ac7e42cef9c
Wellington Santos <wellington.santos@genial.com.vc>
2025-04-12 09:41:52 -0300

    feature: recover password feebback success screen

--- app/lib/ui/auth/recover_password/feedback_success/feedback_success_page.dart
+++ app/lib/ui/auth/recover_password/feedback_success/feedback_success_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:routefly/routefly.dart';
 
 import '../../../../app_widget.dart';
+import '../../../design_system/constants/spaces.dart';
+import '../../../design_system/theme/theme.dart';
+import '../../../design_system/widgets/button_widget.dart';
 
 class FeedbackSuccessPage extends StatefulWidget {
   const FeedbackSuccessPage({super.key});
@@ -14,16 +18,52 @@ class _FeedbackSuccessPageState extends State<FeedbackSuccessPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Center(
         child: Column(
-          spacing: 12.0,
+          spacing: Spaces.l,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Tela de feedback de sucesso'),
-            ElevatedButton(
-              onPressed: () {
-                Routefly.push(routePaths.feed);
-              },
-              child: const Text('OK'),
+            Container(
+              width: Spaces.xxxxl,
+              height: Spaces.xxxxl,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Spaces.xxxxl),
+                border: Border.all(color: context.colors.successLightColor),
+              ),
+              child: Icon(
+                Iconsax.tick_circle,
+                color: context.colors.successLightColor,
+                size: Spaces.xl,
+              ),
+            ),
+            SizedBox(
+              width: 300,
+              child: Text(
+                'Cadastro realizado com sucesso!',
+                textAlign: TextAlign.center,
+                style: context.text.bodyXL18Bold.copyWith(
+                  color: context.colors.whiteColor,
+                ),
+              ),
+            ),
+            Text(
+              'Seja bem-vindo à Flutterando',
+              style: context.text.bodyL16Bold.copyWith(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Container(
+              width: 250,
+              margin: const EdgeInsets.only(top: Spaces.l),
+              child: ButtonWidget.filledPrimary(
+                onPressed: () {
+                  Routefly.navigate(routePaths.feed);
+                },
+                text: 'Começar',
+                padding: const EdgeInsets.symmetric(
+                  vertical: Spaces.xl - Spaces.xs,
+                ),
+              ),
             ),
           ],
         ),
