75c7a5e984685875c83ae33b1990e628ad44eef2
Wellington Santos <wellington.santos@genial.com.vc>
2025-04-12 12:15:19 -0300

    fix: appbar flutterando

--- app/lib/ui/feed/feed_page.dart
+++ app/lib/ui/feed/feed_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../design_system/constants/spaces.dart';
+import '../design_system/widgets/appbar_widget.dart';
 import '../design_system/widgets/link_post_widget.dart';
 import '../design_system/widgets/post_user_widget.dart';
 
@@ -14,21 +15,20 @@ class FeedPage extends StatefulWidget {
 class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Column(
-          spacing: Spaces.m,
-          children: [
-            Text('Tela do feed'),
-            PostUserWidget(),
-            LinkPostWidget(
-              postDescription:
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nisl nunc egestas nunc, eget lacinia nunc nisl eget nunc.',
-              imageUrl:
-                  'https://media.licdn.com/dms/image/v2/D4E22AQG23nwGm5GN9A/feedshare-shrink_2048_1536/B4EZWzWwX4HgAs-/0/1742470843949?e=1747267200&v=beta&t=FnsBSTJQ6zTZn9aSksEC8zFBpH-GBO8yV_NjI0LXhe8',
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBarWidget(),
+      body: Column(
+        spacing: Spaces.m,
+        children: [
+          Text('Tela do feed'),
+          PostUserWidget(),
+          LinkPostWidget(
+            postDescription:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nisl nunc egestas nunc, eget lacinia nunc nisl eget nunc.',
+            imageUrl:
+                'https://media.licdn.com/dms/image/v2/D4E22AQG23nwGm5GN9A/feedshare-shrink_2048_1536/B4EZWzWwX4HgAs-/0/1742470843949?e=1747267200&v=beta&t=FnsBSTJQ6zTZn9aSksEC8zFBpH-GBO8yV_NjI0LXhe8',
+          ),
+        ],
       ),
     );
   }
