85344b66b7033cbdf83bce85cf1118b3a978d505
Wellington Santos <wellington.santos@genial.com.vc>
2025-04-16 09:04:20 -0300

    chore: remove mock folder

--- app/lib/data/services/api/mock_payload/mock_payload.dart
+++
@@ -1,9 +0,0 @@
-const mockPayloadLogin = {
-  'token': '123',
-  'refreshToken': '123',
-  'user': {
-    'firstName': 'jacob',
-    'lastName': 'Moura',
-    'email': 'jacobinho@gmail.com',
-  },
-};
