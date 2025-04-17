2c5dc9bafcd3b168360ebea8c9220cb5643a40c7
Tiago Pinheiro <tiagopinheirobg@gabul.dev>
2025-04-17 17:08:49 -0300

    refactor: extract PostCachedImage widget for better image handling in LinkPostWidget

--- app/lib/ui/design_system/widgets/post_feed_widget/link_post_widget.dart
+++ app/lib/ui/design_system/widgets/post_feed_widget/link_post_widget.dart
@@ -1,55 +1,48 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/spaces.dart';
 import '../../theme/theme.dart';
+import 'post_cached_image_widget.dart';
 
 class LinkPostWidget extends StatelessWidget {
   final String imageUrl;
   final String postDescription;
-  const LinkPostWidget({
-    super.key,
-    required this.imageUrl,
-    required this.postDescription,
-  });
+  const LinkPostWidget({super.key, required this.imageUrl, required this.postDescription});
 
   @override
   Widget build(BuildContext context) {
+    final hasSubtitle = postDescription.isEmpty;
     return Column(
       children: [
         Container(
           height: 330,
           decoration: BoxDecoration(
             border: Border.all(color: Theme.of(context).colors.greyTwo),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(Spaces.m),
-              topRight: Radius.circular(Spaces.m),
+            borderRadius: BorderRadius.only(
+              topLeft: const Radius.circular(Spaces.m),
+              topRight: const Radius.circular(Spaces.m),
+              bottomLeft: Radius.circular(hasSubtitle ? Spaces.m : 0),
+              bottomRight: Radius.circular(hasSubtitle ? Spaces.m : 0),
             ),
-            image: DecorationImage(
-              image: CachedNetworkImageProvider(imageUrl),
-              fit: BoxFit.cover,
-            ),
+            // image: DecorationImage(image: CachedNetworkImageProvider(imageUrl), fit: BoxFit.cover),
           ),
+          child: PostCachedImage(imageUrl: imageUrl, borderRadius: Spaces.m, hasSubtitle: hasSubtitle),
         ),
-        Container(
-          padding: const EdgeInsets.all(Spaces.s),
-          width: double.infinity,
-          decoration: BoxDecoration(
-            border: Border.all(color: Theme.of(context).colors.greyTwo),
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(Spaces.m),
-              bottomRight: Radius.circular(Spaces.m),
+        if (!hasSubtitle)
+          Container(
+            padding: const EdgeInsets.all(Spaces.s),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).colors.greyTwo),
+              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(Spaces.m), bottomRight: Radius.circular(Spaces.m)),
+            ),
+            child: Text(
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              postDescription,
+              style: Theme.of(context).textStyles.bodyM14Bold.copyWith(fontWeight: FontWeight.w400),
             ),
           ),
-          child: Text(
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            postDescription,
-            style: Theme.of(
-              context,
-            ).textStyles.bodyM14Bold.copyWith(fontWeight: FontWeight.w400),
-          ),
-        ),
       ],
     );
   }
---
+++ app/lib/ui/design_system/widgets/post_feed_widget/post_cached_image_widget.dart
@@ -0,0 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class PostCachedImage extends StatelessWidget {
+  final String imageUrl;
+  final String? name;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final double? borderRadius;
+  final String? cacheKey;
+  final bool hasSubtitle;
+  const PostCachedImage({
+    super.key,
+    required this.imageUrl,
+    this.name,
+    this.width,
+    this.height,
+    this.fit,
+    this.backgroundColor,
+    this.textColor,
+    this.borderRadius,
+    this.cacheKey,
+    this.hasSubtitle = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(borderRadius ?? 0),
+        topRight: Radius.circular(borderRadius ?? 0),
+        bottomLeft: hasSubtitle ? Radius.circular(borderRadius ?? 0) : Radius.zero,
+        bottomRight: hasSubtitle ? Radius.circular(borderRadius ?? 0) : Radius.zero,
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        width: width ?? 330,
+        height: height ?? 330,
+        fit: fit ?? BoxFit.cover,
+        cacheKey: cacheKey,
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        placeholderFadeInDuration: const Duration(milliseconds: 100),
+        errorWidget: (context, url, error) => const Center(child: Text('Erro ao carregar imagem')),
+      ),
+    );
+  }
+}
